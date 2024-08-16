f1640ef79689bfc62d55ff74db1abf8f47164001
NI-ICK <cccccccccc55144133@gmail.com>
2024-08-16 21:39:50 +0200

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react'
 import fs from 'fs'
 import path from 'path'
 import dotenv from 'dotenv'
+import vercel from 'vite-plugin-vercel'
 
 dotenv.config()
 
 const isLocal = process.env.NODE_ENV === 'development';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
   server: {
     port: 3000,
     https: isLocal ? {
